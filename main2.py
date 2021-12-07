98dd4038b539db0fe92c847f9ec6f5d6d174d8e0
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2021-12-07 10:13:26 +0100

    hi
    Neueste Updatatete

--- main2.py
+++ main2.py
@@ -103,14 +103,9 @@ def multiple_voter_manipulations(origin_votings, coalition):
     coalition_happiness_i = []
     winners = get_winners(origin_votings)
     all_permutations = list(itertools.permutations(origin_votings[0]))
-    all_permutations_list = []
-    for i in range(len(all_permutations)):
-        all_permutations_list.append(list(all_permutations[i]))
-    all_permutations = all_permutations_list
     for permutation in range(len(all_permutations)):
         temp_votings = origin_votings
         for member in coalition:
-            print(member)
             coalition_votings.append(temp_votings[member])
             coalition_happiness_i.append(happiness(np.array(winners), np.array(temp_votings[member])))
             temp_votings[member] = all_permutations[permutation]
@@ -145,7 +140,7 @@ def create_groups(votings, threshold: int):
                     # to reduce the complexity, the new contributor is only compared to one of the already existing team
                     if get_voter_compatibility(votings[team[0][0]], friend) <= threshold:
                         team_idxs = [i for i in team[0]]
-                        new_teams.append([team_idxs + [j], multiple_voter_manipulations(votings, [team_idxs + [j]])])
+                        new_teams.append([team_idxs + [j], multiple_voter_manipulations(votings, team_idxs + [j])])
         voting_groups += new_teams
         teams = new_teams
     return voting_groups
